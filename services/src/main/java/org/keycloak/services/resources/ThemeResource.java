27a6a6af3794f63a75b33a9666434fc1265c9f6e
Steve Lewis <stelewis@redhat.com>
2022-12-29 14:48:14 -0600

    Use ThemeResource instead.

--- services/src/main/java/org/keycloak/services/resources/ThemeResource.java
+++ services/src/main/java/org/keycloak/services/resources/ThemeResource.java
@@ -16,22 +16,35 @@
  */
 package org.keycloak.services.resources;
 
+import org.jboss.logging.Logger;
 import org.keycloak.common.Version;
 import org.keycloak.common.util.MimeTypeUtil;
 import org.keycloak.encoding.ResourceEncodingHelper;
 import org.keycloak.encoding.ResourceEncodingProvider;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.services.ErrorResponse;
 import org.keycloak.services.ServicesLogger;
+import org.keycloak.services.resources.account.AccountRestService;
 import org.keycloak.services.util.CacheControlUtil;
 import org.keycloak.theme.Theme;
+import org.keycloak.util.JsonSerialization;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Theme resource
@@ -41,8 +54,39 @@ import java.io.InputStream;
 @Path("/resources")
 public class ThemeResource {
 
+    private static final Logger logger = Logger.getLogger(ThemeResource.class);
+
     @Context
     private KeycloakSession session;
+
+    @GET
+    @Path("/localizations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getLocalizations(@QueryParam("realm") String realmName, @QueryParam("themeType") String themeType, @QueryParam("themeName") String themeName, @QueryParam("locale") String localeStr) {
+        Locale locale = Locale.forLanguageTag(localeStr);
+
+        Theme theme;
+        try {
+            theme = session.theme().getTheme(themeName, Theme.Type.valueOf(themeType.toUpperCase()));
+        } catch (IOException var12) {
+            String errorDesc = "Failed to create theme";
+            logger.error(errorDesc, var12);
+            return ErrorResponse.error(errorDesc, Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+        Properties messages = null;
+        try {
+            messages = theme.getMessages(locale);
+        } catch (IOException e) {
+            String errorDesc = "Unable to get messages for locale: " + locale;
+            logger.error(errorDesc, e);
+            return ErrorResponse.error(errorDesc, Response.Status.INTERNAL_SERVER_ERROR);
+        }
+        RealmModel realm = session.realms().getRealmByName(realmName);
+        messages.putAll(realm.getRealmLocalizationTextsByLocale(locale.toLanguageTag()));
+
+        return messagesToJsonString(messages);
+    }
 
     /**
      * Get theme content
@@ -84,6 +128,44 @@ public class ThemeResource {
             ServicesLogger.LOGGER.failedToGetThemeRequest(e);
             return Response.serverError().build();
         }
+    }
+
+    private String messagesToJsonString(Properties props) {
+        if (props == null) return "";
+        Properties newProps = new Properties();
+        for (String prop: props.stringPropertyNames()) {
+            newProps.put(prop, convertPropValue(props.getProperty(prop)));
+        }
+        try {
+            return JsonSerialization.writeValueAsString(newProps);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private String convertPropValue(String propertyValue) {
+        // this mimics the behavior of java.text.MessageFormat used for the freemarker templates:
+        // To print a single quote one needs to write two single quotes.
+        // Single quotes will be stripped.
+        // Usually single quotes would escape parameters, but this not implemented here.
+        propertyValue = propertyValue.replaceAll("'('?)", "$1");
+        propertyValue = putJavaParamsInNgTranslateFormat(propertyValue);
+
+        return propertyValue;
+    }
+
+    // Put java resource bundle params in ngx-translate format
+    // Do you like {0} and {1} ?
+    //    becomes
+    // Do you like {{param_0}} and {{param_1}} ?
+    private String putJavaParamsInNgTranslateFormat(String propertyValue) {
+        final Pattern bundleParamPattern = Pattern.compile("(\\{\\s*(\\d+)\\s*\\})");
+
+        Matcher matcher = bundleParamPattern.matcher(propertyValue);
+        while (matcher.find()) {
+            propertyValue = propertyValue.replace(matcher.group(1), "{{param_" + matcher.group(2) + "}}");
+        }
+
+        return propertyValue;
     }
 
 }
